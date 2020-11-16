24a4a63f1f186a6812da6efe811e8c253a2ae6a1
Samuel Anderson <andersonhenderson45@gmail.com>
2020-11-16 12:23:06 -0500

    done

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var msg = "Hey there!" + ( 9 + 10 );
-
-alert(msg);
-
-if ( true == true ) {
-    console.log(false);
+function sing(song) {
+    console.log(song);
 }
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function checkAge(age) {
+    if (age > 18 || age < 5) {
+        console.log("cool age!");
+    }
+}
+
+sing("yah yeet");
+sing("woo hoo");
+
+console.log(add(2, 3));
+
+checkAge(27);
